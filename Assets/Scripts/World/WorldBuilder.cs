a4a843f01abc565579597919efe0285702842d37
Flan <jamioflan@hotmail.co.uk>
2017-12-02 16:09:32 +0000

    It's ALL fine

--- Assets/Scripts/World/WorldBuilder.cs
+++ Assets/Scripts/World/WorldBuilder.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour
 {
-    public WorldTile floor, hill;
+    public WorldTile floor, hill, edge;
     public WorldTile[,] worldTiles;
     public Material[] biomeMaterials = new Material[Biomes.iNUM_BIOMES];
     public int iWidth = 40, iHeight = 40;
@@ -37,19 +37,19 @@ public class WorldBuilder : MonoBehaviour
 
         worldTiles = new WorldTile[width,height];
 
-        bool[,] tiles = new bool[width, height];
+        int[,] tiles = new int[width, height];
 
         // Borders
         for(int i = 0; i < width; i++)
         {
-            tiles[i, 0] = hill;
-            tiles[i, height - 1] = hill;
+            tiles[i, 0] = 2;
+            tiles[i, height - 1] = 2;
         }
 
         for (int j = 0; j < height; j++)
         {
-            tiles[0, j] = hill;
-            tiles[width - 1, j] = hill;
+            tiles[0, j] = 2;
+            tiles[width - 1, j] = 2;
         }
 
         float fNoiseOffsetX = Random.Range(-10000.0f, 10000.0f);
@@ -60,7 +60,7 @@ public class WorldBuilder : MonoBehaviour
         {
             for(int j = 1; j < height - 1; j++)
             {
-                tiles[i, j] = Mathf.PerlinNoise(fNoiseOffsetX + fHillNoiseScale * (float)i / (float)width, fNoiseOffsetY + fHillNoiseScale * (float)j / (float)height) > 0.5f;
+                tiles[i, j] = Mathf.PerlinNoise(fNoiseOffsetX + fHillNoiseScale * (float)i / (float)width, fNoiseOffsetY + fHillNoiseScale * (float)j / (float)height) > 0.5f ? 1 : 0;
             }
         }
 
@@ -87,16 +87,30 @@ public class WorldBuilder : MonoBehaviour
         {
             for(int j = 0; j < height; j++)
             {
-                worldTiles[i, j] = Instantiate<WorldTile>(tiles[i,j] ? hill : floor, transform);
+                WorldTile tile = null;
+                switch(tiles[i, j])
+                {
+                    case 0: tile = floor; break;
+                    case 1: tile = hill; break;
+                    case 2: tile = edge; break;
+                }
+                worldTiles[i, j] = Instantiate<WorldTile>(tile, transform);
                 worldTiles[i, j].transform.localPosition = new Vector3(i * 10.0f, 0.0f, j * 10.0f);
 
                 // Setup walls
-                if(tiles[i,j])
+                if(tiles[i,j] == 1)
                 {
-                    worldTiles[i, j].walls[(int)Direction.UP].SetActive(j != height - 1 && !tiles[i, j + 1]);
-                    worldTiles[i, j].walls[(int)Direction.RIGHT].SetActive(i != width - 1 && !tiles[i + 1, j]);
-                    worldTiles[i, j].walls[(int)Direction.DOWN].SetActive(j != 0 && !tiles[i, j - 1]);
-                    worldTiles[i, j].walls[(int)Direction.LEFT].SetActive(i != 0 && !tiles[i - 1, j]);
+                    worldTiles[i, j].walls[(int)Direction.UP].SetActive(j != height - 1 && tiles[i, j + 1] == 0);
+                    worldTiles[i, j].walls[(int)Direction.RIGHT].SetActive(i != width - 1 && tiles[i + 1, j] == 0);
+                    worldTiles[i, j].walls[(int)Direction.DOWN].SetActive(j != 0 && tiles[i, j - 1] == 0);
+                    worldTiles[i, j].walls[(int)Direction.LEFT].SetActive(i != 0 && tiles[i - 1, j] == 0);
+                }
+                else if (tiles[i, j] == 2)
+                {
+                    worldTiles[i, j].walls[(int)Direction.UP].SetActive(j != height - 1 && tiles[i, j + 1] < 2);
+                    worldTiles[i, j].walls[(int)Direction.RIGHT].SetActive(i != width - 1 && tiles[i + 1, j] < 2);
+                    worldTiles[i, j].walls[(int)Direction.DOWN].SetActive(j != 0 && tiles[i, j - 1] < 2);
+                    worldTiles[i, j].walls[(int)Direction.LEFT].SetActive(i != 0 && tiles[i - 1, j] < 2);
                 }
 
                 // Setup biome
