b50220be3704e76de3b0a24658116d39401054c0
Rebecca <rdodson94@yahoo.co.uk>
2017-12-02 13:53:30 +0000

    Added blank On Click functions and linked them to UI buttons

--- Assets/Scripts/Workbench.cs
+++ Assets/Scripts/Workbench.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Workbench : MonoBehaviour {
-
+public class Workbench : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		// TODO: Populate all the text and images with the right stuff
+	}
+
+	// OnClick functions
+	public void Listbox_Decrement()
+	{
+
+	}
+
+	public void Listbox_Increment()
+	{
+
+	}
+
+	public void BuildItem()
+	{
+
 	}
 }
