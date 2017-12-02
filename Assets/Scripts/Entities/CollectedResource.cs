5e4b75b095cb678035d2c7040d0a5b226f36e788
j0nburr <jonathan.thomas.burr@cern.ch>
2017-12-02 13:50:18 +0000

    Adding resource enum

--- Assets/Scripts/Entities/CollectedResource.cs
+++ Assets/Scripts/Entities/CollectedResource.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ResourceType { Scandium, Red, Green, Blue }
+
 public class CollectedResource : MonoBehaviour {
 
 	// Use this for initialization
