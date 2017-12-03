ce2815b6d85e8ad5774526e10da747174be0c5e2
j0nburr <jonathan.thomas.burr@cern.ch>
2017-12-03 14:22:34 +0000

    The workbench steals things

---
+++ Assets/Scripts/World/Forcefield.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forcefield : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("I'm here!");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision!");
+        PlayerBehaviour player = other.GetComponent<PlayerBehaviour>();
+        if (player != null)
+        {
+            while( player.collector.collectedResources.Count > 0)
+            {
+                CollectedResource resource = player.collector.collectedResources[0];
+                resource.SetOwner(Core.GetCore().theWorkbench.resourceReceiver);
+                resource.followDistance = 0.0f;
+            }
+        }
+    }
+}
