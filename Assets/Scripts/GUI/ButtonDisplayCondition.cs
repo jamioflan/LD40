a6bf2f7c827bafeb49577cadaccc7b0c8f1d093d
Flan <jamioflan@hotmail.co.uk>
2017-12-03 19:14:42 +0000

    disabled build button

---
+++ Assets/Scripts/GUI/ButtonDisplayCondition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class displayCond : UnityEvent<Button>
+{
+
+}
+
+
+public class ButtonDisplayCondition : MonoBehaviour
+{
+    public displayCond dispCond;
+
+    // Use this for initialization
+    void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+    private void OnGUI()
+    {
+        Button button = gameObject.GetComponent<Button>();
+        dispCond.Invoke(button);
+    }
+}
